bbf69e21d9b36f9959ed2f58b170954dbec22ae6
Robin de Mourat <robin.demourat@gmail.com>
2014-12-02 11:31:10 +0100

    basic version for personal pages

--- app/scripts/controllers/personne.js
+++ app/scripts/controllers/personne.js
@@ -14,10 +14,12 @@ angular.module('derCleanApp')
 
 
   	$rootScope.$watch('annuaireData', function(){
-    	if($rootScope.annuaireData)
+    	if($rootScope.annuaireData){
     		$scope.membre = findMember(slugActive);
-
-    	console.log($scope.membre);
+    		formatMembre();
+    		console.log($scope.membre);
+    	}
+    		
     });
 
     function findMember(slug){
@@ -28,5 +30,42 @@ angular.module('derCleanApp')
     	}
     }
 
+    function formatMembre(){
+    	$scope.membre.site = $scope.membre['urlpersonalwebsiteaboutyourresearch'];
+    	if($scope.membre.phonepublic){
+    		if(($scope.membre.phonepublic+"").charAt(0) == '6')
+    			$scope.membre.phonepublic = '+33' + $scope.membre.phonepublic;
+    		else if(($scope.membre.phonepublic+"").charAt(0) == '0')
+    			$scope.membre.phonepublic = '+33' + $scope.membre.phonepublic.substring(1, $scope.membre.phonepublic.length);
+    	}
+
+    	//sites
+    	var sites= $scope.membre['urlotherwebsitesseparatedbycomasacademiatwitteretc.'].split(',');
+    	var outputSites = [];
+    	for(var i in sites){
+    		var site = sites[i],out = {};
+    		out.url = site.trim();
+    		if(site.indexOf('facebook')> -1)
+    			out.text = "Facebook";
+    		else if(site.indexOf('twitter')> -1)
+    			out.text = "Twitter";
+    		else if(site.indexOf('linkedin')> -1)
+    			out.text = "LinkedIn";
+    		else if(site.indexOf('academia')> -1)
+    			out.text = "Academia";
+    		else out.text = site.trim();
+    		outputSites.push(out);
+    	}
+    	$scope.membre.sites = outputSites;
+    }
+
     $scope.pageClass = 'page-personne';
+
+
+    $scope.contactVisible = function(){
+    	if($scope.membre)
+    	return $scope.membre["e-mailpublic"] != "" || $scope.membre.phonepublic != ""
+    	|| $scope.membre.urlpersonalwebsiteaboutyourresearch != "";
+    	else return false;
+    }
   });
