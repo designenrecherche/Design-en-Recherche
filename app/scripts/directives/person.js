755f2084445e7c391ada18dd6709580355923e7f
Robin de Mourat <robin.demourat@gmail.com>
2015-02-15 14:51:42 +0100

    Added person directive

---
+++ app/scripts/directives/person.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name derCleanApp.directive:person
+ * @description
+ * # person
+ */
+ angular.module('derCleanApp')
+ .directive('person', function ($rootScope, $document) {
+  	//slugify function, identical to the gspreadsheet script's one
+
+  	function slugify(value) {
+
+  		value = value.toLowerCase();
+  		value = value.replace(/[^\w\s-]/g, '');
+  		value = value.replace(/\s+/g, '-');
+
+  		return value;
+
+  	}
+  	//find someone annuaire's id basing on her naturally spelled name and surname
+  	function findInAnnuaire(text, annuaire){
+  		console.log(text);
+  		for(var i in annuaire){
+  			var name = annuaire[i].name;
+  			if(slugify(text).indexOf(slugify(name)) > -1){
+  				return annuaire[i].identifiant;
+  			}
+  		}
+  	}
+  	return {
+      restrict: 'AE',//tagname or attribute
+      link: function postLink(scope, element, attrs) {
+
+      	//search the name of the person and try to make a link to it in the annuaire
+      	$rootScope.$watch('annuaireData', function(){
+      		if($rootScope.annuaireData){
+      			var id = findInAnnuaire(element.text(), $rootScope.annuaireData);
+      			if(id){
+      				element.attr('id', id);
+      				var text = element.text();
+      				var a = angular.element('<a href="#personne/'+id+'">'+text+'</a>');
+      				element.text('');
+      				element.append(a);
+      			}
+      		}
+      	})
+
+      	element.on('mouseover', mouseover);
+      	function mouseover(){
+      		console.log('mouseover');
+      		if(element.attr('id'))
+      			$rootScope.activeMemberId = element.attr('id');
+      	}
+      	element.on('mouseout', function(){
+      		$document.off('mouseover', mouseover);
+      		$rootScope.activeMemberId = "";
+      	})
+      }
+  };
+});
