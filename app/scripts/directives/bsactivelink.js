d2816e90fb0c5f7247486cc96d83288d947760a0
Robin de Mourat <robin.demourat@gmail.com>
2015-02-21 17:40:12 +0100

    fixed nav active when complex locations

--- app/scripts/directives/bsactivelink.js
+++ app/scripts/directives/bsactivelink.js
@@ -15,22 +15,14 @@ angular.module('derCleanApp')
             //after the route has changed
             scope.$on("$routeChangeSuccess", function () {
                 var lis = angular.element(elem).find('li');
-                var routeBase = $location.path();
+                var routeBase = $location.path().split('/')[1];
                 angular.forEach(lis, function(li, key){
-                  var href = $(li).find('a').attr('href');
-                  console.log(href, routeBase);
-                })
-                //cleaning links
-                /* $('navbar li').removeClass('active');
-                 //select active links
-                var selectors = ['navbar li > [href="/#' + $location.path() + '"]',
-                'navbar li > [href="#' + $location.path().substring(1) + '"]',
-                    'navbar li > [href="#' + $location.path() + '"]', //html5: false
-                'navbar li > [href="' + $location.path() + '"]']; //html5: true
-
-                $(elem).find(selectors.join(',')) //find the matching link
-                .parent('li').addClass('active') //add active class to the matching element
-                .siblings('li').removeClass('active'); //remove it from the sibling elements*/
+                  var href = $(li).find('a').attr('href').substring(1);
+                  if(href === routeBase && !$(li).hasClass('active'))
+                    $(li).addClass('active');
+                  else if (href != routeBase && $(li).hasClass('active'))
+                    $(li).removeClass('active');
+                });
             });
         }
     };
